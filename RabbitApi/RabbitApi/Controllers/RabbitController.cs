c09dfff3a26f6ae28eecb0b37bd16078ab504370
Cristian Covaliu <cristian.covaliu@gmail.com>
2020-02-21 16:59:46 +0200

    update1

--- RabbitApi/RabbitApi/Controllers/RabbitController.cs
+++ RabbitApi/RabbitApi/Controllers/RabbitController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,32 +13,69 @@ namespace RabbitApi.Controllers
     [ApiController]
     public class RabbitController : ControllerBase
     {
+
+        private static List<Rabbit> rabbits = new List<Rabbit>();
+        public RabbitController()
+        {
+            Rabbit r1 = new Rabbit() { Age = 4, EyeColor = EyeColors.Black, FurColor = FurColors.Black, Genders = Genders.Female};
+            Rabbit r2 = new Rabbit() { Age = 3, EyeColor = EyeColors.Blue, FurColor = FurColors.Brown, Genders = Genders.Male };
+            Rabbit r3 = new Rabbit() { Age = 2, EyeColor = EyeColors.Red, FurColor = FurColors.White, Genders = Genders.Female};
+
+            if (!rabbits.Any())
+            {
+                rabbits.Add(r1);
+                rabbits.Add(r2);
+                rabbits.Add(r3);
+            }
+        } 
         // GET: api/Rabbit
         [HttpGet]
         public IEnumerable<Rabbit> Get()
         {
-            Rabbit r1 = new Rabbit(Rabbit.FurColors.Grey, Rabbit.EyeColors.Black, Rabbit.Gender.Female);
-            Rabbit r2 = new Rabbit(Rabbit.FurColors.Black, Rabbit.EyeColors.Blue, Rabbit.Gender.Male);
-            Rabbit r3 = new Rabbit(Rabbit.FurColors.Brown, Rabbit.EyeColors.Red, Rabbit.Gender.Male);
-            
-            List<Rabbit> rabbit = new List<Rabbit>();
-            rabbit.Add(r1);
-            rabbit.Add(r2);
-            rabbit.Add(r3);
-            return rabbit;
+            return rabbits;
         }
 
         // GET: api/Rabbit/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Rabbit Get(int id)
         {
-            return "value";
+            try
+            {
+                Rabbit item = rabbits[id];
+                
+                return item;
+
+            }
+            catch (Exception)
+            {
+
+                throw new HttpResponseException(System.Net.HttpStatusCode.NotFound);
+            }
+            
+            
+
+
+            //try
+            //{
+            //    return rabbits[id - 1];
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw new HttpResponseException(HttpStatusCode.NotFound); ;
+            //} 
         }
 
         // POST: api/Rabbit
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Rabbit model)
         {
+            if (ModelState.IsValid)
+            {
+                rabbits.Add(model);
+                return Ok();
+            }
+            return BadRequest();
         }
 
         // PUT: api/Rabbit/5
@@ -49,6 +87,33 @@ namespace RabbitApi.Controllers
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
+        {
+        }
+    }
+
+    [Serializable]
+    internal class HttpResponseException : Exception
+    {
+        private object notFound;
+
+        public HttpResponseException()
+        {
+        }
+
+        public HttpResponseException(object notFound)
+        {
+            this.notFound = notFound;
+        }
+
+        public HttpResponseException(string message) : base(message)
+        {
+        }
+
+        public HttpResponseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected HttpResponseException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
--- RabbitApi/RabbitApi/Models/Rabbit.cs
+++ RabbitApi/RabbitApi/Models/Rabbit.cs
@@ -1,53 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RabbitApi.Models
 {
+    public enum FurColors
+    {
+        White = 1,
+        Brown,
+        Black,
+        Grey
+    }
+    public enum EyeColors
+    {
+        Blue = 1,
+        Black,
+        Red
+    }
+    public enum Genders
+    {
+        Male = 1,
+        Female
+    }
     public class Rabbit
     {
-        public enum FurColors
-        {
-            White,
-            Brown,
-            Black,
-            Grey
-        }
-        public enum EyeColors
-        {
-            Blue,
-            Black,
-            Red
-        }
-        public enum Gender
-        {
-            Male,
-            Female
-        }
-        private FurColors furColor;
-        private EyeColors eyeColor;
-        private Gender gender;
-        private readonly DateTime dateOfBirth;
-        public Rabbit(FurColors furColors, EyeColors eyeColor, Gender gender)
-        {
-            dateOfBirth = DateTime.Now;
-        }
-        public void Move()
-        {
-            Console.WriteLine("The rabbit is moving left and right like crazy.");
-        }
-        public void Sleep()
-        {
-            Console.WriteLine("The rabbit is sleeping like a rock.");
-        }
-        public void Eat()
-        {
-            Console.WriteLine("The rabbit is eating some food");
-        }
-        public int Age
-        {
-            get { return (DateTime.Now - dateOfBirth).Days / 365; }
-        }
+
+        [Required]
+        [EnumDataType(typeof(FurColors))]
+        public FurColors FurColor { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(EyeColors))]
+        public EyeColors EyeColor { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(Genders))]
+        public Genders Genders { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
     }
+
+    
 }
